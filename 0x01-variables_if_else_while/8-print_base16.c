4f9d697705018bfd07569172ef9b3c9ae6b5e1c2
josefgeda <dxrjos@gmail.com>
2022-03-13 20:43:49 +0000

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,12 @@
 int main(void)
 {
 	char ch;
-	
-	for (ch = 48 ; ch <= 57 ; ch++)
-		putchar(ch);
-	for (ch = 97 ; ch <= 102 ; ch++)
-		putchar(ch);
+		for (ch = 48 ; ch <= 57 ; ch++)
+			putchar(ch);
+		for (ch = 97 ; ch <= 102 ; ch++)
+			putchar(ch);
 	
 	putchar('\n');
-	return 0;
+	return (0);
 }
 
