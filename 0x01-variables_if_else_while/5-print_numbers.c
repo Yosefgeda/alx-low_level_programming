a20f4e59d919b6a2ce5f3bbeeb399a940ee8acf7
josefgeda <dxrjos@gmail.com>
2022-03-13 18:56:36 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - to excute printing of single digit numbers
+ * Return: prints all single digit numbers starting from 0
+ */
+int main (void)
+{
+	int n = 0;
+	for (n =0 ; n < 10 ;n++)
+		printf("%d",n);
+	printf("\n");
+	return (0);
+
+
+
+}
