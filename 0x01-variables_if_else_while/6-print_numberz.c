ebcd72fe790c2cdefec17e3f0e4d79ac2c7bcf6c
josefgeda <dxrjos@gmail.com>
2022-03-13 19:27:46 +0000

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to print numbers
+ * Return: prints numbers form 0 to 9
+ */
+int main(void)
+{
+	int n = 48;
+
+	for (n = 48 ; n < 58 ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+
+
+}
