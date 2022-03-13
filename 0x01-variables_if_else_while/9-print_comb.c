1a56092dcc014c7462a627d8f26b0c451f690cbb
josefgeda <dxrjos@gmail.com>
2022-03-13 21:02:39 +0000

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - combinations
+ * Return: numbers from 0 to 9 separeted with commas
+ */
+
+int main(void)
+{
+	int ch;
+	
+		for (ch = 48 ; ch <= 57 ; ch++)
+		{
+			putchar(ch);
+			if (ch == 57)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
+
