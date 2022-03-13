250b37aa47947062e3d773189412bcf8e5c12f19
josefgeda <dxrjos@gmail.com>
2022-03-13 16:58:32 +0000

    no q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - to exexute alphabet without e and q
+ * Return: alphabet in lower case without e and q
+ */
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e') continue;
+		if (ch == 'q') continue;
+		else putchar(ch);
+	}
+	putchar('\n');
+    	return 0;
+}
+
