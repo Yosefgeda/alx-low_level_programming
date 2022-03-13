deed99e52da934e2ccd3e72222bef82a01f6a1ce
josefgeda <dxrjos@gmail.com>
2022-03-13 16:09:39 +0000

    print alphabet lower

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - to pront lower case alphabets
+ * Return: prints the alphabets and a new line
+ */
 int main ()
 {
 	char A;
-
 	for (A='a' ; A <- 'z'; A++)
+	{
 		putchar(A);
-		putchar('\n');
-	
-		
-		return 0;
+	}
+	putchar('\n');
+	return 0;
 
 
 
