09856ca65be54d568c1356ab3e60e20acd1361fc
josefgeda <dxrjos@gmail.com>
2022-03-15 16:23:22 +0000

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - print _putchar
+ * Return: value
+ */
+
+int main(void)
+{
+
+	printf("_putchar");
+	return (0);
+
+
+
+
+}
