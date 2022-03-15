b24db62e76a01befbb20d033c5e16938845b0144
josefgeda <dxrjos@gmail.com>
2022-03-15 16:33:34 +0000

    task0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: value from a to z
+ */
+
+int main(void)
+{
+        void print_alphabet(void);
+	{
+		char ch;
+		for (ch = 'a' ; ch<= 'z' ; ch++)
+			putchar(ch);
+			return(ch);
+	}
+        return (0);
+
+
+
+
+}
+
