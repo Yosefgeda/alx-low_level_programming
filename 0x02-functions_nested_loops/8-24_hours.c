32a702b523a96377a6509804515cd79a59401ed8
josefgeda <dxrjos@gmail.com>
2022-03-16 20:12:14 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 	int a;
 	int b;
 
-	for (a = 0 ; a <= 23 ; a++)
+	for (a = 0 ; a < 24 ; a++)
 	{
-	for (b = 0 ; b <= 59 ; b++)
+	for (b = 0 ; b < 60 ; b++)
 	{
 	_putchar (a / 10 + '0');
 	-putchar (a % 10 + '0');
