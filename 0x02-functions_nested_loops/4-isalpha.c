c6de122712ca41306677e4aef47ba9508161bc96
josefgeda <dxrjos@gmail.com>
2022-03-16 14:54:31 +0000

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - checks if value alphabet
+ * @c: parameter
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+        if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+                return (1);
+        else
+                return (0);
+}
