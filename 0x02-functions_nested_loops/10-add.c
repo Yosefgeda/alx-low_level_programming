a9689253c655bf1e6d981a1f9242f6670392ade7
josefgeda <dxrjos@gmail.com>
2022-03-16 20:54:36 +0000

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - checks if sign of number
+ * @a: input integer
+ * @b: input integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
