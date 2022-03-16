118516d743d002ac30f3fa5ff2d7d03bffec1e24
josefgeda <dxrjos@gmail.com>
2022-03-16 13:03:49 +0000

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/** 
+ * int _islower - lower alphabet
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	char l ='a';
+
+	if (l>= 'a' && l <= 'z')
+		putchar('1');
+	else putchar('0');
+	putchar('\n');
+
+        return (c);
+}
+
