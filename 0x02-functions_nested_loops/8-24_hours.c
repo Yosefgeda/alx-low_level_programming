4667286971bc93b68f6311bd16b8ebdf6c88db04
josefgeda <dxrjos@gmail.com>
2022-03-16 20:25:09 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * jack_bauer - checks if sign of number
- * @n: prints last digit
+ *
  * Return: the last digit
  */
 void jack_bauer(void)
@@ -15,8 +15,10 @@ void jack_bauer(void)
 	for (b = 0 ; b < 60 ; b++)
 	{
 	_putchar (a / 10 + '0');
+	_putchar (a % 10 + '0');
 	_putchar (':');
 	_putchar (b / 10 + '0');
+	_putchar (b % 10 + '0')
 	_putchar ('\n');
 
 	}
