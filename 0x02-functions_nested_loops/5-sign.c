0001faf99d680d2388d0992aa990a17250141471
josefgeda <dxrjos@gmail.com>
2022-03-16 17:42:31 +0000

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@
 
 int print_sign(int n)
 {
-	if (n>0)
+	if (n > 0)
 		{
 		putchar('0');
 		return(1);
 		}
 		
-		else if (n==0)
+		else if (n == 0)
 		{
-			putchar(0);
+			putchar('0');
 			return(0);
 		}
 		else
