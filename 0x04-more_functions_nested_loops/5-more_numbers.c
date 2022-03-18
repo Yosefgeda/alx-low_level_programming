5efd66e382917913f255741211158ef85ec675f7
josefgeda <dxrjos@gmail.com>
2022-03-18 22:32:47 +0000

    10x14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,32 @@ void more_numbers(void)
 {
 
 int i;
-int n;
+int first;
+int second;
+int result;
 
-for (i = 0 ; i < 10 ; i++)
-{
-for (n = 0 ; n <= 14 ; n++)
-	printf("%d", n);
-	putchar('\n');
+i=0;
+result=0;
+while (i < 10)
+	{
+		while (result <= 14)
+		{	if (result < 10)
+			{
+				second = result;
+			}
+			else
+			{
+				first = result / 10;
+				second = result % 10;
+				_putchar (first + '0');
+			}
+		_putchar (second + '0');
+		result++;
+		}	
+	i++;
+	result = 0;
+	_putchar ('\n');
+	}
+
 
 }
