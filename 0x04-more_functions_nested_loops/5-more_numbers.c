123022695a3dfd91da6e63c6044c2e1724dfd808
josefgeda <dxrjos@gmail.com>
2022-03-18 22:16:38 +0000

    10x14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - table
+ * Return: 0
+ */
+void more_numbers(void)
+{
+
+int i;
+int n;
+
+for (i = 0 ; i < 10 ; i++)
+{
+for (n = 0 ; n <= 9 ; n++)
+	printf("%d", n);
+	putchar('\n');
+
+}
