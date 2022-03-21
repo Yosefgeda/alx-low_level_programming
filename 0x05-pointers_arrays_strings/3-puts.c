dae2a7201be4339ac92851d9cb47280b025246a9
josefgeda <dxrjos@gmail.com>
2022-03-21 18:14:47 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function
+ * @str: string
+ * Return: string
+ */
+void _puts(char *str)
+{
+int i;
+
+for (i = 0 ; str[i] != '\n' ; i++)
+{
+	printf("%c\n", i);
+}
+}
