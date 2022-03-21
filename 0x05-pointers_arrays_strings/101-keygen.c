67e0626374a00653b1fe3ab3422863f00f2dec1f
josefgeda <dxrjos@gmail.com>
2022-03-21 21:11:57 +0000

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-int i, len;
+int i;
 
-for (i = 0 ; i < len ; i++)
+for (i = 0 ; i < 1000 ; i++)
 {
 int k = rand() % 128;
 if ((k >= 48 && k <= 57) || (k >=65 && k <= 90) || (k >=97 && k <= 122) || (k >=35 && k <=37) || (k == 64))
@@ -18,4 +18,5 @@ printf("%c", k);
 }
 else { i--;}
 }
+return (0);
 }
