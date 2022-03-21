07a211dc504c89ecf4663099987b5ed1297b1432
josefgeda <dxrjos@gmail.com>
2022-03-21 18:54:33 +0000

    rev puts

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - function
+ * @str: string
+ * Return: string
+ */
+void _puts(char *str)
+{
+int i;
+
+for (i = '\0' ; str[i] > '\0' ; i--)
+{
+        _putchar(str[i]);
+}
+        _putchar('\n');
+}
+
