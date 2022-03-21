6945644701bc4c5b3ed30937707ebd1a263dd800
josefgeda <dxrjos@gmail.com>
2022-03-21 19:13:41 +0000

    rev puts

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,10 +14,9 @@ for (length = 0 ; s[length] != '\0' ; length++)
 {
 
 }
-for (i = length ; s[length] != '\0' ; i--)
+for (i = length - 1 ; i >=0 ; i--)
 {
 _putchar(s[i]);
 }
 _putchar('\n');
 }
-
