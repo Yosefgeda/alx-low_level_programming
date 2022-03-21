7bf0c9b410e93f55a942aadee552db97fb0d7fd4
josefgeda <dxrjos@gmail.com>
2022-03-21 20:05:53 +0000

    a to i

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - string to integer
+ * @s: variable
+ * Return: value
+ */
+int _atoi(char *s)
+{
+int i = 0, sum = 0;
+
+while (s[i] != '\0')
+{
+	if (s[i] < 48 || s[i > 57])
+	{
+		printf("no value");
+	}
+
+	else
+	{
+		sum = sum *10 + (s[i] - 48);
+	}
+	i++;
+}
+
+
+}
