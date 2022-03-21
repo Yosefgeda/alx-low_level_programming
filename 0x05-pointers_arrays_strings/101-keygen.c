feff6f93469f63f22ac77d87111ce77fa86d6ae8
josefgeda <dxrjos@gmail.com>
2022-03-21 20:58:51 +0000

    password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - random
+ * Return: 0
+ */
+int main(void)
+{
+int i, len;
+
+for (i = 0 ; i < len ; i++)
+{
+int k + rand() % 128;
+if ((k >= 48 && k <= 57) || (k >=65 && k <= 90) || (k >=97 && k <= 122) || (k >=35 && k <=37) || (k == 64))
+{
+printf("%c", k);
+}
+else { i--;}
+}
+}
