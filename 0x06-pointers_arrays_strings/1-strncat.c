5a44eea96af13bf4fd92a7a78e5d6fe9acc22dfa
josefgeda <dxrjos@gmail.com>
2022-03-23 21:04:45 +0000

    str n cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code
+ * @dest: char variable
+ * @src: char variable
+ * @n: int variable
+ * Return: value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+
+}
