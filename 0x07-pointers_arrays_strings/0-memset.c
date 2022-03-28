220f58f031a224c015f9b6de7a75c6e3cbeac672
josefgeda <dxrjos@gmail.com>
2022-03-28 20:10:26 +0100

    memory space

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - constant byte
+ * @s: variable
+ * @b: variable
+ * @n: variable
+ * Return: value
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0 ; i < n ; i++)
+{
+*(s + i) = b;
+}
+return (s);
+}
