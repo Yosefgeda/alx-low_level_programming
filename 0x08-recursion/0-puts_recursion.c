199d9891b785a54bc8ae18d36c1dc74772321760
josefgeda <dxrjos@gmail.com>
2022-03-30 18:20:12 +0100

    prints string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string
+ * @s: string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+if (s <= '\0')
+return (_putchar("%s\n", s));
+}
