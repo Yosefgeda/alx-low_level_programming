23b76bd850a2edff6979114929092150d2b25e50
josefgeda <dxrjos@gmail.com>
2022-04-04 21:29:33 +0100

    prints file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,11 @@
  * @argv: argument string
  * Return: int
  */
-int main (int argc, char **argv[])
+int main (int argc, char *argv[])
 {
-printf("%s\n", *argv[0]);
+int i;
+
+for (i = 0 ; i < argc ; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
