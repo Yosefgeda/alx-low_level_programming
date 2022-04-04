2b58c47393edabdae83b4eb84de4240e43b8fdd9
josefgeda <dxrjos@gmail.com>
2022-04-04 20:19:21 +0100

    prints file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument string
+ * Return: int
+ */
+int main (int argc, char *argv[])
+{
+int i;
+
+for (i = 0 : i =! '\0' ; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
