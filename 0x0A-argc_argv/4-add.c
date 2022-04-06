650ad551a217dabfb7cb3d4f59df4785fcb388ce
josefgeda <dxrjos@gmail.com>
2022-04-06 15:12:14 +0100

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,22 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-int sum = 0;
-
-for (i = 0 ; i < argc ; i++)
-if (!isdigit(argv[i]))
+int i, j, add = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
-else if (i = 0)
-{
-printf("0\n");
 }
-else
-{
-sum += atoi(argv[i]);
+add += atoi(argv[i]);
 }
-printf("%d\n", sum);
+printf("%d\n", add);
+return (0);
 }
