f903ff38ee734f00d2345decccc99d8db574a522
josefgeda <dxrjos@gmail.com>
2022-04-06 09:27:39 +0100

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/** main - function
+ * @argc: argument to main
+ * @argv: argumrnt to main
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int mul = 1;
+if (argc > 1)
+{
+for (i = 1 ; i < argc ; i++)
+mul = mul * argv[1];
+printf("%d\n", mul);
+}
+else
+{
+printf("error");
+return (1);
+}
+return (0);
+}
