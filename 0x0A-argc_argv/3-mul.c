abe4e5f0d7d9f96c4eb9aeff5d5a44d8e8f0daf8
josefgeda <dxrjos@gmail.com>
2022-04-06 09:54:48 +0100

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,20 +7,11 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-int mul = 1;
-
-if (argc = 3)
-{
-for (i = 1 ; i < argc ; i++)
-mul = mul * atoi(argv[i]);
-}
-printf("%d\n", mul);
-
-else
-{
-printf("error");
-return (1);
-}
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
