6011c3163e8d1ed66c8092ed7b947e9003deb533
josefgeda <dxrjos@gmail.com>
2022-04-06 09:57:20 +0100

    multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-/** main - function
+/**
+ * main - function
  * @argc: argument to main
  * @argv: argumrnt to main
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }
