21e7b0eddc8c65f5ca5081dc6d96bf42651dcc56
josefgeda <dxrjos@gmail.com>
2022-04-12 12:31:11 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,13 @@
  * @owner: third member
  * Description: structure for registering the three values
  */
-
+#ifndef DOG_H
+#define DOG_H
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
 
