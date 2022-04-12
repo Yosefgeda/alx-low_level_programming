632d4dc82d31413c609eb2d130e0e05f97a19d0d
josefgeda <dxrjos@gmail.com>
2022-04-12 12:03:45 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - file for a dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: structure for registering the three values
+ */
+
+struct dog
+{
+char *name;
+float *age;
+char *owner;
+};
